b031299e8cee4cf9e12acf5a633d3bd0c9147ea0
lauraigo <lauraibarragomez@gmail.com>
2022-01-21 10:41:40 +0100

    agregar, nombre de columna y quitar missing

--- script.R
+++ script.R
@@ -4,6 +4,33 @@ tir <- MLDataR::thyroid_disease
 
 write.csv(tir, file = "dataset_tiroides.csv")
 
+sessioninfo()
 
-  
-  
+#order
+
+head(tir)
+sort(tir$patient_age)
+
+tir2 <-  tir[order(tir$patient_age),] 
+tir2
+
+# quitar los missing
+
+tir_completo <- tir[complete.cases(tir),]
+
+
+#aggregate
+
+edad_tir <- aggregate(tir_completo$patient_age, list(tir_completo$ThryroidClass), mean)
+
+edad_tir2 <- aggregate(tir_completo$patient_age, list(tir_completo$ThryroidClass, tir_completo$patient_gender), mean)
+
+?aggregate
+
+#poner nombre a las columnas
+
+names(edad_tir) <- c("Tiroides", "Media")
+names(edad_tir)
+
+names(edad_tir2) <- c("Tiroides", "Género", "Media")
+edad_tir2
